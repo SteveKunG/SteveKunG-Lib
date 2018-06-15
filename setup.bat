4b6cfdabbd9a1bcd78f40b5a0c85cb1b06e27295
SteveKunG <mccommander_minecraft@hotmail.com>
2018-06-16 00:35:36 +0700

    Intitial files

---
+++ setup.bat
@@ -0,0 +1,2 @@
+set GRADLE_USER_HOME=M:\Modding\SteveKunG-Lib\1.13\.gradle_data
+gradlew setupDecompWorkspace eclipse
---
+++ src/main/java/stevekung/mods/stevekunglib/utils/client/ClientUtils.java
@@ -0,0 +1,66 @@
+package stevekung.mods.stevekunglib.utils.client;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.Style;
+import net.minecraftforge.client.ClientCommandHandler;
+import net.minecraftforge.fml.common.FMLCommonHandler;
+import net.minecraftforge.fml.relauncher.Side;
+import stevekung.mods.stevekunglib.utils.JsonUtils;
+
+public class ClientUtils
+{
+    public static boolean isClient()
+    {
+        return FMLCommonHandler.instance().getSide() == Side.CLIENT;
+    }
+
+    public static boolean isEffectiveClient()
+    {
+        return FMLCommonHandler.instance().getEffectiveSide() == Side.CLIENT;
+    }
+
+    public static boolean isShiftKeyDown()
+    {
+        return GuiScreen.isShiftKeyDown();
+    }
+
+    public static boolean isControlKeyDown()
+    {
+        return GuiScreen.isCtrlKeyDown();
+    }
+
+    public static void setOverlayMessage(String message)
+    {
+        Minecraft.getMinecraft().ingameGUI.setOverlayMessage(message, false);
+    }
+
+    public static void setOverlayMessage(ITextComponent component)
+    {
+        Minecraft.getMinecraft().ingameGUI.setOverlayMessage(component.getFormattedText(), false);
+    }
+
+    public static void printClientMessage(String text)
+    {
+        ClientUtils.printClientMessage(JsonUtils.create(text));
+    }
+
+    public static void printClientMessage(String text, Style color)
+    {
+        ClientUtils.printClientMessage(JsonUtils.create(text).setStyle(color));
+    }
+
+    public static void printClientMessage(ITextComponent component)
+    {
+        if (Minecraft.getMinecraft().player != null)
+        {
+            Minecraft.getMinecraft().player.sendMessage(component);
+        }
+    }
+
+    public static void registerCommand(ClientCommandBase command)
+    {
+        ClientCommandHandler.instance.registerCommand(command);
+    }
+}
