aa1f3160607940f98d4298c1e214f63066e9c0ae
SteveKunG <mccommander_minecraft@hotmail.com>
2018-06-30 01:31:05 +0700

    Update workspace

--- build.bat
+++ build.bat
@@ -4,4 +4,4 @@ rd /s /q "M:\Modding\SteveKunG-Lib\1.13\build\libs"
 rd /s /q "M:\Modding\SteveKunG-Lib\1.13\build\resources"
 rd /s /q "M:\Modding\SteveKunG-Lib\1.13\build\sources"
 set GRADLE_USER_HOME=M:\Modding\SteveKunG-Lib\1.13\.gradle_data
-gradlew buildJar
+gradlew build
--- update_api.bat
+++ update_api.bat
@@ -1,2 +1,3 @@
 xcopy /s /Y "M:\Modding\SteveKunG-Lib\1.13\src" "M:\Modding\MorePlanets\1.13\dependencies"
 xcopy /s /Y "M:\Modding\SteveKunG-Lib\1.13\src" "M:\Modding\Indicatia\1.13\dependencies"
+xcopy /s /Y "M:\Modding\SteveKunG-Lib\1.13\src" "M:\Modding\YouTubeChat\1.12.2\dependencies"
