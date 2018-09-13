66ae26049092218fa2c2cd0b987d269ac76c3f65
SteveKunG <mccommander_minecraft@hotmail.com>
2018-09-13 13:58:20 +0700

    Bump to 1.0.3

--- src/main/java/stevekung/mods/stevekunglib/core/SteveKunGLib.java
+++ src/main/java/stevekung/mods/stevekunglib/core/SteveKunGLib.java
@@ -23,7 +23,7 @@ public class SteveKunGLib
     public static final String MOD_ID = "stevekung's_lib";
     protected static final int MAJOR_VERSION = 1;
     protected static final int MINOR_VERSION = 0;
-    protected static final int BUILD_VERSION = 2;
+    protected static final int BUILD_VERSION = 3;
     public static final String VERSION = SteveKunGLib.MAJOR_VERSION + "." + SteveKunGLib.MINOR_VERSION + "." + SteveKunGLib.BUILD_VERSION;
     protected static final String FORGE_VERSION = "after:forge@[14.23.4.2705,);";
     protected static final String JSON_URL = "https://raw.githubusercontent.com/SteveKunG/VersionCheckLibrary/master/stevekung's_lib_version.json";
