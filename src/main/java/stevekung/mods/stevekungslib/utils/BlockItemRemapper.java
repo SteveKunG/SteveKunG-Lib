04909e02ddc71fab70003fe412478902d89256f6
SteveKunG <mccommander_minecraft@hotmail.com>
2019-08-01 20:20:24 +0700

    Some cleanup

--- src/main/java/stevekung/mods/stevekungslib/utils/BlockItemRemapper.java
+++ src/main/java/stevekung/mods/stevekungslib/utils/BlockItemRemapper.java
@@ -3,44 +3,45 @@ package stevekung.mods.stevekungslib.utils;
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.event.RegistryEvent.MissingMappings.Mapping;
 import stevekung.mods.stevekungslib.core.SteveKunGLib;
 
 public class BlockItemRemapper
 {
     public static void remapBlock(RegistryEvent.MissingMappings<Block> event, String modid, String oldName, Block block)
     {
-        event.getMappings().forEach(mappings ->
+        for (Mapping<Block> mappings : event.getMappings())
         {
             if (mappings.key.getNamespace().equals(modid) && mappings.key.getPath().equals(oldName))
             {
                 mappings.remap(block);
                 SteveKunGLib.LOGGER.info("Remapping 'Block' from {} to {}", mappings.key, block.getRegistryName());
             }
-        });
+        }
     }
 
     @SuppressWarnings("deprecation")
     public static void remapItem(RegistryEvent.MissingMappings<Item> event, String modid, String oldName, Block block)
     {
-        event.getMappings().forEach(mappings ->
+        for (Mapping<Item> mappings : event.getMappings())
         {
             if (mappings.key.getNamespace().equals(modid) && mappings.key.getPath().equals(oldName))
             {
                 mappings.remap(Item.getItemFromBlock(block));
                 SteveKunGLib.LOGGER.info("Remapping 'ItemBlock' from {} to {}", mappings.key, block.getRegistryName());
             }
-        });
+        }
     }
 
     public static void remapItem(RegistryEvent.MissingMappings<Item> event, String modid, String oldName, Item item)
     {
-        event.getMappings().forEach(mappings ->
+        for (Mapping<Item> mappings : event.getMappings())
         {
             if (mappings.key.getNamespace().equals(modid) && mappings.key.getPath().equals(oldName))
             {
                 mappings.remap(item);
                 SteveKunGLib.LOGGER.info("Remapping 'Item' from {} to {}", mappings.key, item.getRegistryName());
             }
-        });
+        }
     }
 }
