813d1fb5a499fe8d4a7bba1329c54f75fd3d99a6
SteveKunG <mccommander_minecraft@hotmail.com>
2019-09-14 12:35:35 +0700

    Update to latest forge 1.12.2 and bump to 1.1.5

--- src/main/java/stevekung/mods/stevekunglib/core/SteveKunGLib.java
+++ src/main/java/stevekung/mods/stevekunglib/core/SteveKunGLib.java
@@ -27,9 +27,9 @@ public class SteveKunGLib
     public static final String MOD_ID = "stevekung's_lib";
     protected static final int MAJOR_VERSION = 1;
     protected static final int MINOR_VERSION = 1;
-    protected static final int BUILD_VERSION = 4;
+    protected static final int BUILD_VERSION = 5;
     public static final String VERSION = SteveKunGLib.MAJOR_VERSION + "." + SteveKunGLib.MINOR_VERSION + "." + SteveKunGLib.BUILD_VERSION;
-    protected static final String FORGE_VERSION = "after:forge@[14.23.5.2838,);";
+    protected static final String FORGE_VERSION = "after:forge@[14.23.5.2847,);";
     protected static final String JSON_URL = "https://raw.githubusercontent.com/SteveKunG/VersionCheckLibrary/master/stevekung's_lib_version.json";
     protected static final String CERTIFICATE = "@FINGERPRINT@";
     private static final String URL = "https://minecraft.curseforge.com/projects/stevekungs-lib";
