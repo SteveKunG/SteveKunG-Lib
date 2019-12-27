ea3a4a96652ba40ede741731bae748cc8a5668c9
SteveKunG <mccommander_minecraft@hotmail.com>
2019-12-27 17:31:44 +0700

    Fix TextComponent in client command

--- src/main/java/com/stevekung/stevekungslib/utils/client/command/IClientSuggestionProvider.java
+++ src/main/java/com/stevekung/stevekungslib/utils/client/command/IClientSuggestionProvider.java
@@ -1,10 +1,10 @@
 package com.stevekung.stevekungslib.utils.client.command;
 
 import net.minecraft.command.ISuggestionProvider;
-import net.minecraft.util.text.TextComponent;
+import net.minecraft.util.text.ITextComponent;
 
 public interface IClientSuggestionProvider extends ISuggestionProvider
 {
-    void sendFeedback(TextComponent text);
-    void sendErrorMessage(TextComponent text);
+    void sendFeedback(ITextComponent text);
+    void sendErrorMessage(ITextComponent text);
 }
