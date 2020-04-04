51a6b4b5f5867a292a7d263cc4e4d3c420921761
SteveKunG <mccommander_minecraft@hotmail.com>
2020-04-04 14:00:03 +0700

    Fixed lag spike when in the Enchantment Table GUI, Closed #6

--- src/main/java/com/stevekung/stevekungslib/mixin/MixinFontResourceManager.java
+++ src/main/java/com/stevekung/stevekungslib/mixin/MixinFontResourceManager.java
@@ -2,13 +2,10 @@ package com.stevekung.stevekungslib.mixin;
 
 import java.util.Collections;
 
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Mutable;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import com.stevekung.stevekungslib.core.SteveKunGLib;
 import com.stevekung.stevekungslib.utils.client.ClientUtils;
@@ -25,15 +22,10 @@ import net.minecraft.util.ResourceLocation;
 @Mixin(FontResourceManager.class)
 public abstract class MixinFontResourceManager
 {
-    @Shadow
-    @Final
-    @Mutable
-    private TextureManager textureManager;
-
-    @Inject(method = "getFontRenderer(Lnet/minecraft/util/ResourceLocation;)Lnet/minecraft/client/gui/FontRenderer;", at = @At("HEAD"))
-    private void getFontRenderer(ResourceLocation id, CallbackInfoReturnable<FontRenderer> info)
+    @Inject(method = "<init>(Lnet/minecraft/client/renderer/texture/TextureManager;Z)V", at = @At("RETURN"))
+    private void initUnicodeFont(TextureManager textureManager, boolean forceUnicodeFont, CallbackInfo info)
     {
-        ClientUtils.unicodeFontRenderer = new FontRenderer(this.textureManager, new Font(this.textureManager, new ResourceLocation(SteveKunGLib.MOD_ID, "unicode")));
+        ClientUtils.unicodeFontRenderer = new FontRenderer(textureManager, new Font(textureManager, new ResourceLocation(SteveKunGLib.MOD_ID, "unicode")));
         IGlyphProvider provider = new UnicodeTextureGlyphProvider.Factory(new ResourceLocation("font/glyph_sizes.bin"), "minecraft:font/unicode_page_%s.png").create(Minecraft.getInstance().getResourceManager());
         ClientUtils.unicodeFontRenderer.setGlyphProviders(Collections.singletonList(provider));
         SteveKunGLib.LOGGER.info("Unicode Font initialized");
