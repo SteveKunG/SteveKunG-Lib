e00f2ccac2ceddaaea6547b7282e05e46f395fbd
SteveKunG <mccommander_minecraft@hotmail.com>
2020-04-04 14:37:51 +0700

    Fixed conflict with OptiForge

--- src/main/java/com/stevekung/stevekungslib/mixin/MixinGameRenderer.java
+++ src/main/java/com/stevekung/stevekungslib/mixin/MixinGameRenderer.java
@@ -5,12 +5,10 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import com.mojang.blaze3d.matrix.MatrixStack;
 import com.stevekung.stevekungslib.utils.client.EventHooksClient;
 
-import net.minecraft.client.renderer.ActiveRenderInfo;
 import net.minecraft.client.renderer.GameRenderer;
 
 @Mixin(GameRenderer.class)
@@ -19,9 +17,9 @@ public class MixinGameRenderer
     @Shadow
     private int rendererUpdateCount;
 
-    @Inject(method = "renderWorld(FJLcom/mojang/blaze3d/matrix/MatrixStack;)V", at = @At(value = "INVOKE", target = "net/minecraft/client/renderer/GameRenderer.applyBobbing(Lcom/mojang/blaze3d/matrix/MatrixStack;F)V", shift = At.Shift.AFTER), locals = LocalCapture.CAPTURE_FAILHARD)
-    private void injectCameraEvent(float partialTicks, long nanoTime, MatrixStack stack, CallbackInfo info, boolean flag, ActiveRenderInfo activerenderinfo, MatrixStack matrixstack)
+    @Inject(method = "renderWorld(FJLcom/mojang/blaze3d/matrix/MatrixStack;)V", at = @At(value = "INVOKE", target = "net/minecraft/client/renderer/GameRenderer.hurtCameraEffect(Lcom/mojang/blaze3d/matrix/MatrixStack;F)V", shift = At.Shift.AFTER))
+    private void injectCameraEvent(float partialTicks, long finishTimeNano, MatrixStack matrixStack, CallbackInfo info)
     {
-        EventHooksClient.onCameraTransform(this.rendererUpdateCount, partialTicks, matrixstack);
+        EventHooksClient.onCameraTransform(this.rendererUpdateCount, partialTicks, matrixStack);
     }
 }
