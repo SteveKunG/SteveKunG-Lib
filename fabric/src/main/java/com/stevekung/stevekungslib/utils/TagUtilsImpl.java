e948cc38723ee28961c2ca14add1aab66ff91d10
SteveKunG <mccommander_minecraft@hotmail.com>
2021-03-11 01:39:42 +0700

    TagUtils for fabric

--- fabric/src/main/java/com/stevekung/stevekungslib/utils/TagUtilsImpl.java
+++ fabric/src/main/java/com/stevekung/stevekungslib/utils/TagUtilsImpl.java
@@ -1,37 +1,21 @@
 package com.stevekung.stevekungslib.utils;
 
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.StaticTags;
 import net.minecraft.tags.Tag;
+import net.minecraft.tags.TagContainer;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.alchemy.Potion;
-import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.entity.BlockEntityType;
 
-//TODO Tag for fabric
 public class TagUtilsImpl
 {
     public static Tag.Named<Block> createBlockTag(String modid, String name)
     {
-        return null;
+        return StaticTags.create(new ResourceLocation(modid, "block"), TagContainer::getBlocks).bind(name);
     }
 
     public static Tag.Named<Item> createItemTag(String modid, String name)
     {
-        return null;
-    }
-
-    public static Tag.Named<Enchantment> createEnchantmentTag(String modid, String name)
-    {
-        return null;
-    }
-
-    public static Tag.Named<Potion> createPotionTag(String modid, String name)
-    {
-        return null;
-    }
-
-    public static Tag.Named<BlockEntityType<?>> createTileEntityTag(String modid, String name)
-    {
-        return null;
+        return StaticTags.create(new ResourceLocation(modid, "item"), TagContainer::getItems).bind(name);
     }
 }
