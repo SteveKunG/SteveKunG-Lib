6d1a2a105576fc50d4a56ec9ca2f02c7930a2587
SteveKunG <mccommander_minecraft@hotmail.com>
2021-05-17 13:46:40 +0700

    Added short methods for LanguageProvider

--- forge/src/main/java/com/stevekung/stevekungslib/data/LanguageProviderBase.java
+++ forge/src/main/java/com/stevekung/stevekungslib/data/LanguageProviderBase.java
@@ -1,11 +1,16 @@
 package com.stevekung.stevekungslib.data;
 
+import org.apache.commons.lang3.text.WordUtils;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.level.block.Block;
 import net.minecraftforge.common.data.LanguageProvider;
 
+@SuppressWarnings("deprecation")
 public abstract class LanguageProviderBase extends LanguageProvider
 {
     public LanguageProviderBase(DataGenerator generator, String modid, String locale)
@@ -34,5 +39,25 @@ public abstract class LanguageProviderBase extends LanguageProvider
     protected void add(DamageSource source, String name)
     {
         this.add("death.attack." + source.getMsgId(), name);
+    }
+
+    protected void add(Block key)
+    {
+        this.add(key.getDescriptionId(), WordUtils.capitalize(key.getRegistryName().getPath().replace("_", " ")));
+    }
+
+    protected void add(Item key)
+    {
+        this.add(key.getDescriptionId(), WordUtils.capitalize(key.getRegistryName().getPath().replace("_", " ")));
+    }
+
+    protected void add(EntityType<?> key)
+    {
+        this.add(key.getDescriptionId(), WordUtils.capitalize(key.getRegistryName().getPath().replace("_", " ")));
+    }
+
+    protected void add(MobEffect key)
+    {
+        this.add(key.getDescriptionId(), WordUtils.capitalize(key.getRegistryName().getPath().replace("_", " ")));
     }
 }
