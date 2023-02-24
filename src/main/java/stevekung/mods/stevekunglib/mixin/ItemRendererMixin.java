1302fa89e80c2180b0ea1de2afb8fec94cbd383f
Wasinthorn Suksri <mccommander_minecraft@hotmail.com>
2023-02-24 16:08:10 +0700

    Fix injection for ItemRendererMixin

--- src/main/java/stevekung/mods/stevekunglib/mixin/ItemRendererMixin.java
+++ src/main/java/stevekung/mods/stevekunglib/mixin/ItemRendererMixin.java
@@ -1,29 +1,18 @@
 package stevekung.mods.stevekunglib.mixin;
 
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.ItemRenderer;
 import stevekung.mods.stevekunglib.utils.client.EventHooksClient;
 
 @Mixin(ItemRenderer.class)
 public class ItemRendererMixin
 {
-    @Shadow
-    @Final
-    Minecraft mc;
-
-    @Inject(method = "renderOverlays(F)V", cancellable = true, at = @At(value = "INVOKE", target = "net/minecraft/client/entity/EntityPlayerSP.isSpectator()Z", shift = At.Shift.AFTER))
+    @Inject(method = "renderOverlays", at = @At(value = "INVOKE", target = "net/minecraft/client/entity/EntityPlayerSP.isInsideOfMaterial(Lnet/minecraft/block/material/Material;)Z"))
     private void stevekunglib$injectFirstPersonViewOverlayEvent(float partialTicks, CallbackInfo info)
     {
-        if (!this.mc.player.isSpectator())
-        {
-            EventHooksClient.onRenderFirstPersonViewOverlay();
-        }
+        EventHooksClient.onRenderFirstPersonViewOverlay();
     }
 }
