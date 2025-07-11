6570b7270faacd0be2bd90d056a8cdbfd49649d4
SteveKunG <mccommander_minecraft@hotmail.com>
2025-07-11 17:27:25 +0700

    buildscript: tag injection wip [skip ci]

---
+++ src/main/java/com/stevekung/lib/core/Tags.java
@@ -0,0 +1,8 @@
+package com.stevekung.lib.core;
+
+public class Tags
+{
+    public static final String VERSION = "";
+    public static final String MOD_ID = "";
+    public static final String MOD_NAME = "";
+}
