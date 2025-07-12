23254be330d9c424023da352ab73cb5f44e4070e
SteveKunG <mccommander_minecraft@hotmail.com>
2025-07-12 22:24:29 +0700

    ci: tags injection is now working

--- src/main/java/com/stevekung/lib/core/Tags.java
+++
@@ -1,8 +0,0 @@
-package com.stevekung.lib.core;
-
-public class Tags
-{
-    public static final String VERSION = "";
-    public static final String MOD_ID = "";
-    public static final String MOD_NAME = "";
-}
